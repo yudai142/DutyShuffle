bbe36035de53064ccc0cc6ab8a3a1815a2f3c487
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-11-14 14:14:45 +0900

    アプリレイアウト作成・役割分担

---
+++ component/sidebar_component.php
@@ -0,0 +1,23 @@
+<body>
+  <div class="sidebar">
+    <header></header>
+    <ul>
+      <li>
+        <a href="#">トップ</a>
+      </li>
+      <li>
+        <a href="#">割り当て</a>
+      </li>
+      <li>
+        <a href="#">登録・編集</a>
+      </li>
+      <li>
+        <a href="#">オプション</a>
+      </li>
+      <li>
+        <a href="#">ダウンロード</a>
+      </li>
+    </ul>
+  </div>
+  <main>
+    <header><?php echo $title; ?></header>
---
+++ public/top.php
@@ -0,0 +1,3 @@
+<?php require_once "../component/head_component.php" ?>
+<link rel="stylesheet" href="../src/css/work_list.css" />
+<?php $title = "トップ"; require_once "../component/sidebar_component.php"; ?>
