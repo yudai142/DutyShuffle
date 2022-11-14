4e18190f9b37c34905b9e1d0d80a228d74abb05e
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-11-14 23:39:01 +0900

    ファイル名修正

--- component/sidebar_component.php
+++ component/sidebar_component.php
@@ -1,23 +1,22 @@
-<body>
-  <div class="sidebar">
-    <header></header>
-    <ul>
-      <li>
-        <a href="#">トップ</a>
-      </li>
-      <li>
-        <a href="#">割り当て</a>
-      </li>
-      <li>
-        <a href="#">登録・編集</a>
-      </li>
-      <li>
-        <a href="#">オプション</a>
-      </li>
-      <li>
-        <a href="#">ダウンロード</a>
-      </li>
-    </ul>
-  </div>
-  <main>
-    <header><?php echo $title; ?></header>
+<div class="sidebar">
+  <header></header>
+  <ul>
+    <li>
+      <a href="#">トップ</a>
+    </li>
+    <li>
+      <a href="#">割り当て</a>
+    </li>
+    <li>
+      <a href="#">登録・編集</a>
+    </li>
+    <li>
+      <a href="#">オプション</a>
+    </li>
+    <li>
+      <a href="#">ダウンロード</a>
+    </li>
+  </ul>
+</div>
+<main>
+  <header><?php echo $title; ?></header>
