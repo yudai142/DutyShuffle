425cc2f1a0173daf02e18cbb4155e91ce8d68725
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-11-16 14:23:07 +0900

    雛形作成

---
+++ public/allocation.php
@@ -0,0 +1,3 @@
+<?php require_once "../component/head_component.php" ?>
+<link rel="stylesheet" href="../src/css/allocation.css" />
+<?php $title = "割り当て"; require_once "../component/sidebar_component.php"; ?>
