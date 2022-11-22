9eb610d3cd7e946c17bec91164a489a6f89576d2
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-11-22 13:29:26 +0900

    Update ajax.js

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -40,6 +40,53 @@ $(function(){
     // return false
   }
   getAllWork();
+
+  $('#submit_member').on('click',function(){
+    if($(this).val()){
+      $.ajax({
+          type: "POST",
+          url: "../classes/ajax.php",
+          datatype: "json",
+          data: {
+              "type": 'member_update',
+              "last_name" : $('#last_name').val(),
+              "first_name" : $('#first_name').val(),
+              "kana_name" : $('#kana_name').val(),
+              "archive" : Number($('#member_archive').prop("checked"))
+          },
+          success: function(data) {
+            $('#member_result').html("<p>" + data[0].last_name + data[0].first_name + "("+data[0].kana_name + ")" + data[0].archive + "を変更しました。</p>");
+            $('#member_show_result').find('h1').append("<li><button class='md-btn' data-target='modal-member' value=" + data[0].id + ">" + data[0].last_name + "　" + data[0].first_name + "</button><li>");
+          },
+          error: function(data) {
+              console.log("通信失敗");
+              console.log(data);
+          }
+      });
+    }else{
+      $.ajax({
+          type: "POST",
+          url: "../classes/ajax.php",
+          datatype: "json",
+          data: {
+              "type": 'member_add',
+              "last_name" : $('#last_name').val(),
+              "first_name" : $('#first_name').val(),
+              "kana_name" : $('#kana_name').val(),
+              "archive" : Number($('#member_archive').prop("checked"))
+          },
+          success: function(data) {
+            $('#member_result').html("<p>" + data[0].last_name + data[0].first_name + "("+data[0].kana_name + ")" + data[0].archive + "を登録しました。</p>");
+            $('#member_show_result').append("<li><button class='md-btn' data-target='modal-member' value=" + data[0].id + ">" + data[0].last_name + "　" + data[0].first_name + "</button><li>");
+          },
+          error: function(data) {
+              console.log("通信失敗");
+              console.log(data);
+          }
+      });
+    }
+    // return false
+  });
   
   $('#submit_work').on('click',function(){
     $.ajax({
