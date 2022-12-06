0b35bedf4128e8b85135cc43a2807a9f18c09fcc
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-12-06 14:03:45 +0900

    割り当てページ雛形構成作成

--- public/allocation.php
+++ public/allocation.php
@@ -1,3 +1,18 @@
 <?php require_once "../component/head_component.php" ?>
 <link rel="stylesheet" href="../src/css/allocation.css" />
 <?php $title = "割り当て"; require_once "../component/sidebar_component.php"; ?>
+<table border="1">
+  <tbody style="display:flex;">
+    <tr bgcolor="pink">
+      <th bgcolor="yellow" style="display:block;">見出し</th><td style="display:block;">テキスト</td><td style="display:block;">テキスト</td>
+    </tr>
+    <tr bgcolor="pink">
+      <th bgcolor="yellow" style="display:block;">見出し</th><td style="display:block;">テキスト</td><td style="display:block;">テキスト</td><td style="display:block;">テキスト<br>四日前</td>
+    </tr>
+  </tbody>
+</table>
+<ul>
+  <li>あ</li>
+  <li>い</li>
+  <li>う</li>
+</ul>
