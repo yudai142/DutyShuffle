36b67e4556add391ae722115cfe7c2ae1c316ad5
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-12-16 10:41:44 +0900

    コーディングファイルを集約&indexファイルから行けるように調整

--- public/allocation.php
+++ public/allocation.php
@@ -2,7 +2,34 @@
 <link rel="stylesheet" href="../src/css/allocation.css" />
 <link rel="stylesheet" href="../src/css/select.css" />
 <?php $title = "割り当て"; require_once "../component/sidebar_component.php"; ?>
+<?php
+date_default_timezone_set('Asia/Tokyo');
 
+//前月・次月リンクが選択された場合は、GETパラメーターから年月を取得
+if(isset($_GET['ym'])){ 
+    $ym = date("Y-m-d",strtotime($_GET['ym']));
+    $select_date = date('Y-m-d', strtotime($_GET['ym']));
+    $timestamp = strtotime($ym);
+}else{
+    //今月の年月を表示
+    $ym = date('Y-m-d');
+    //タイムスタンプ（どの時刻を基準にするか）を作成し、フォーマットをチェックする
+    //strtotime('Y-m-01')
+    $timestamp = strtotime($ym); 
+    if($timestamp === false){//エラー対策として形式チェックを追加
+        //falseが返ってきた時は、現在の年月・タイムスタンプを取得
+        $ym = date('Y-m-d');
+        $timestamp = strtotime($ym);
+        $select_date = date('Y-m-d',$timestamp);
+    }
+}
+//今月の日付　フォーマット　例）2020-10-2
+$today = date('Y-m-d');
+//前月・次月の年月を取得
+//strtotime(,基準)
+$prev = date('Y-m-d', strtotime('-1 day', $timestamp));
+$next = date('Y-m-d', strtotime('+1 day', $timestamp));
+?>
 <div class="date-form">
   <div class="icon-left">
     <a href="?ym=<?php echo $prev; ?>">
--- public/top.php
+++ public/top.php
@@ -2,6 +2,34 @@
 <link rel="stylesheet" href="../src/css/top.css" />
 <link rel="stylesheet" href="../src/css/select.css" />
 <?php $title = "トップ"; require_once "../component/sidebar_component.php"; ?>
+<?php
+date_default_timezone_set('Asia/Tokyo');
+
+//前月・次月リンクが選択された場合は、GETパラメーターから年月を取得
+if(isset($_GET['ym'])){ 
+    $ym = date("Y-m-d",strtotime($_GET['ym']));
+    $select_date = date('Y-m-d', strtotime($_GET['ym']));
+    $timestamp = strtotime($ym);
+}else{
+    //今月の年月を表示
+    $ym = date('Y-m-d');
+    //タイムスタンプ（どの時刻を基準にするか）を作成し、フォーマットをチェックする
+    //strtotime('Y-m-01')
+    $timestamp = strtotime($ym); 
+    if($timestamp === false){//エラー対策として形式チェックを追加
+        //falseが返ってきた時は、現在の年月・タイムスタンプを取得
+        $ym = date('Y-m-d');
+        $timestamp = strtotime($ym);
+        $select_date = date('Y-m-d',$timestamp);
+    }
+}
+//今月の日付　フォーマット　例）2020-10-2
+$today = date('Y-m-d');
+//前月・次月の年月を取得
+//strtotime(,基準)
+$prev = date('Y-m-d', strtotime('-1 day', $timestamp));
+$next = date('Y-m-d', strtotime('+1 day', $timestamp));
+?>
 <div class="date-form">
   <div class="icon-left">
     <a href="?ym=<?php echo $prev; ?>">
@@ -16,12 +44,7 @@
       <i class="fas fa-arrow-alt-circle-right fa-2x"></i>
     </a>
   </div>
-  <?php if(isset($member)):?>
-    <span><a href="logout.php" class="logout-button red-button">ログアウト</a></span>
-    <span><a href="list.php" class="list-button gray-button">予約リスト</a></span>
-  <?php else:?>
-    <span style="position: relative;"><a href="login.php" class="login-button blue-button">割り当てへ</a></span>
-  <?php endif;?>
+  <span style="position: relative;"><a href="./allocation.php?ym=<?php echo $_REQUEST["ym"]; ?>" class="login-button blue-button">割り当てへ</a></span>
 </div>
 
 <div class="main_visual">
@@ -44,7 +67,7 @@
     <div class="join_header">
       <h1>メンバー</h1>
       <div class="button_collection">
-        <span class="btn yellow">クリア</span><!-- /.btn yellow -->
+        <!-- <span class="btn yellow">クリア</span> -->
         <span class="btn green md-btn" data-target="modal-select">参加</span><!-- /.btn green -->
       </div><!-- /.button_collection -->
     </div><!-- /.join_header -->
