1f8ce54e944294dbce0762555964e04a6e0b90c1
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-03-13 16:27:01 +0900

    worksheetテーブル追加

---
+++ prisma/migrations/20260313000001_remove_shuffle_option_interval/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `interval` on the `shuffle_option` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "shuffle_option" DROP COLUMN "interval";
---
+++ prisma/migrations/20260313000002_create_worksheet/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "worksheet" (
+    "id" SERIAL NOT NULL,
+    "interval" INTEGER NOT NULL,
+
+    CONSTRAINT "worksheet_pkey" PRIMARY KEY ("id")
+);
